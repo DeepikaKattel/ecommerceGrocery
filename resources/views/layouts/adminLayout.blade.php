a49d74f9eb35e7bc4b740fea7b30750c71702276
Deepika <deepika.kattel80@gmail.com>
2020-10-22 17:07:52 +0545

    Changed logo in admin panel

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -64,9 +64,9 @@
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="/" class="brand-link">
-            <img src="/assets/images/logo.png" class="brand-image img-circle elevation-3"
+            <img src="/images/logo.png" class="brand-image img-circle elevation-3"
                 style="opacity: .8">
-            <span class="brand-text font-weight-light">Easy Shopping Nepal</span>
+            <span class="brand-text font-weight-light">Community Mart</span>
         </a>
 
         <!-- Sidebar -->
